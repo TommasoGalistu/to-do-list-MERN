136c708840a7ee1cd3e137e7f4e3b7055fd0c7dd
Tommy <tommasogalistu92@gmail.com>
2025-03-10 18:08:38 +0100

    feat: procedure of register complete

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,5 +1,7 @@
-const bycrypt = require('bycrypt');
+const bcrypt = require('bcrypt');
 const User = require('../model/userModel');
+const TIME_TO_REPEAT = process.env.TIME_TO_REPEAT
+const EXTRA_SALT = process.env.EXTRA_SALT
 
 const userController = {
     create: async (req, res) =>{
@@ -10,9 +12,10 @@ const userController = {
                 return res.status(400).json({error: "Email già presente"})
             }
 
-            const passwordBcrypt = await bycrypt
-            const user = new User(req.body);
+            const passwordBcrypt = await bcrypt.hash(req.body.password + EXTRA_SALT, TIME_TO_REPEAT);
+            const user = new User({...req.body, password: passwordBcrypt});
             await user.save();
+
             res.status(200).json(user);
         }catch(error){
             res.status(400).json({error: error.message})
